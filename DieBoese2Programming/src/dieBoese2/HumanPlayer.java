700005dde0324ab8f06eeb3eeadde63b562e7ffc
b1 <b1@LAPTOP-C821H540.Speedport_W_724V_Typ_A_05011603_06_003>
2020-05-28 17:52:59 +0200

    erste Methoden erstellt

--- DieBoese2Programming/src/dieBoese2/HumanPlayer.java
+++ DieBoese2Programming/src/dieBoese2/HumanPlayer.java
@@ -1,5 +1,24 @@
 package dieBoese2;
 
-public class HumanPlayer extends Player {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-}
+public class HumanPlayer extends Player {
+ 
+	public HumanPlayer(char figure){
+		super(figure);
+ }
+	public String getMyMove()
+	{ String coordinate;
+	var in = new BufferedReader(new InputStreamReader(System.in));
+	try {
+		coordinate = in.readLine();
+	// ACHTUNG könnte Endlosschleife werden
+	} catch (IOException e) {
+		return getMyMove();  
+
+	}
+		return coordinate;
+		}
+	}
--- DieBoese2Programming/src/dieBoese2/Player.java
+++ DieBoese2Programming/src/dieBoese2/Player.java
@@ -1,5 +1,46 @@
 package dieBoese2;
 
-public abstract class Player {
+import java.util.Scanner;
 
+/**
+ * 
+ * @author Floris Wittner 1921233
+ *  @version 1.0.0
+ *  
+ *   */
+public abstract class Player {
+	char figure;
+
+Player(char figure) {
+	 this.figure = figure;
+ }
+ protected Board blockSpace(Board board) {
+	
+	 
+	System.out.println("Welches Feld wollen Sie blockieren?");
+	 if(board.isValidMove) {
+		board.changeField(getMyMove(),)
+	}else 
+	{
+	System.out.println("Dieses Feld, kann nicht blockiert werden, wählen Sie ein neues Feld aus");
+	
+	}
+	
+	 return board;
+	 
+ }
+ protected Board makeMove(Board board) {
+	 if(board.isValidMove) {
+			
+		}else 
+		{
+			
+		}
+		 
+		 return board;
+	 
+ }
+ private void printGiveMeMove() {
+	 
+ }
 }
