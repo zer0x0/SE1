8b6ac275cc7b8ad7a60ad9cf9e85c0edcbc6d74b
thanhxt <thanhtran_xuan@hotmail.de>
2020-05-29 16:15:25 +0200

    Game Version 0.1.0

--- DieBoese2Programming/src/dieBoese2/Game.java
+++ DieBoese2Programming/src/dieBoese2/Game.java
@@ -1,26 +1,43 @@
 package dieBoese2;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Game {
 
-	public static void main(String[] args) {
-	 Game game = new Game();
-	}
-	protected static String readInput() {
-		String input;
-		
-		try(var in = new BufferedReader(new InputStreamReader(System.in))) {
-			input = in.readLine();
-		// ACHTUNG könnte Endlosschleife werden
-		} catch (IOException e) {
-			System.out.println("Eingabe ist falsch!");
-			return readInput();  
-		}		
-			return input;
-			
-	}
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+    protected static String readInput() {
+        String input;
+
+        try(var in = new BufferedReader(new InputStreamReader(System.in))) {
+            input = in.readLine();
+            // ACHTUNG könnte Endlosschleife werden
+        } catch (IOException e) {
+            System.out.println("Eingabe ist falsch!");
+            return readInput();
+        }
+        return input;
+
+    }
+    Menu menu = new Menu();
+    //menu settings
+    menu.menuloop();
+
+    /**
+     * generates the Players
+     * (first PvP)
+     */
+
+    Player p1 = new HumanPlayer('X');
+    Player p2 = new HumanPlayer('O');
+
+    /**
+     * creates the Board
+     */
+    Board board = new Board(menu.getBoardSize);
 
 }
