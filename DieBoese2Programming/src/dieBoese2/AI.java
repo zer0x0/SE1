d49f128f6de8672b0972ebbc766706e70aa4885b
b1 <b1@LAPTOP-C821H540.Speedport_W_724V_Typ_A_05011603_06_003>
2020-06-05 15:06:29 +0200

    up

--- DieBoese2Programming/src/dieBoese2/AI.java
+++ DieBoese2Programming/src/dieBoese2/AI.java
@@ -1,10 +1,60 @@
 package dieBoese2;
 
 public class AI extends Player {
-
-	AI(char figure) {
+int difficulty;
+	AI(char figure, int difficulty) {
 		super(figure);
-		// TODO Auto-generated constructor stub
+		this.difficulty = difficulty;
+	}
+	public static void main(String[] args) {
+		AI test = new AI('B',3);
+		System.out.println(test.convertCoordinate(1, 2));
+		System.out.println(test.convertCoordinate(14, 15));
+	}
+
+	@Override
+	protected void blockSpace(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void makeMove(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected int getDepth() {
+		return difficulty;
+	}
+	
+	protected int[] generateBlockMove(Board board) {
+		int blockedField[] = new int[2];
+		int x = board.boardstate.length/2;
+		int y = board.boardstate.length/2;
+		while(!board.isValidMove(convertCoordinate(x, y))) {
+			
+		}
+		return blockedField;
+		
+	}
+	private String convertCoordinate(int x, int y) {
+		String coordinates;
+		char xChar = (char)(x+96);
+		if (y<10) {
+		char yChar = (char)(y+48);
+		coordinates = String.valueOf(xChar)+ String.valueOf(yChar);
+		
+		}else{
+			char yChar = (char)((y%10)+48);
+			System.out.println(yChar);
+			char yyChar = (char)((y/10)+48);
+			System.out.println(yyChar);
+			coordinates = String.valueOf(xChar)+ String.valueOf(yyChar)+ String.valueOf(yChar);
+		}
+		
+		return coordinates;
+		
 	}
 
 }
