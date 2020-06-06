18bc6920f91a670cb35c4f5e81d3bde7c3a88f9b
serda <serda@Serdar-PC>
2020-06-06 23:01:10 +0200

    Game aktualisiert in Testing

---
+++ DieBoese2Testing/src/dieBoese2/GameTest.java
@@ -0,0 +1,17 @@
+package dieBoese2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void test() {
+		
+	
+		assertEquals("Text", Game.readInput());
+		
+	}
+
+}
