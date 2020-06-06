b26948dc4bb4ab73177a80f5e6b3fb8bdb6fa1c0
robert <robert@v-monitor>
2020-06-06 14:08:26 +0200

    Tests neu geschrieben, anpassung an verschiedene Feldgroessen

--- DieBoese2Testing/src/dieBoese2/BoardTest.java
+++ DieBoese2Testing/src/dieBoese2/BoardTest.java
@@ -15,15 +15,25 @@ import org.junit.Test;
 
 public class BoardTest {
 
-	int boardWidth = 15;
+	final int BOARD_MAX_SIZE = 19;
+	final int BOARD_MIN_SIZE = 15;
+
+	private static char[][] makeHelpState(Board board) {
+		char[][] helpstate = new char[board.getBoardstate().length][board.getBoardstate()[0].length];
+		for (int i = 0; i < helpstate.length; i++)
+			for (int j = 0; j < helpstate[i].length; j++)
+				helpstate[i][j] = ' ';
+		;
+		return helpstate;
+	}
 
 	/**
 	 * Test-ID Board1
 	 */
 	@Test
 	public void printBoardTest1() {
-		Board board = new Board(boardWidth);
-		for (int i = 15; i < 20; i++) {
+		Board board;
+		for (int i = BOARD_MIN_SIZE; i <= BOARD_MAX_SIZE; i++) {
 			board = new Board(i);
 			board.printBoard();
 		}
@@ -35,16 +45,19 @@ public class BoardTest {
 	 */
 	@Test
 	public void printBoardTest2() {
-		Board board = new Board(boardWidth);
-		char[][] helpstate = new char[board.getBoardstate().length][board.getBoardstate()[0].length];
-		helpstate[0][0] = 'X';
-		helpstate[helpstate.length - 1][0] = 'X';
-		helpstate[0][helpstate[0].length - 1] = 'X';
-		helpstate[helpstate.length - 1][helpstate[0].length - 1] = 'X';
-		helpstate[3][8] = 'X';
-		helpstate[8][11] = 'X';
-		board.setBoardstate(helpstate);
-		board.printBoard();
+		Board board;
+		for (int i = BOARD_MIN_SIZE; i <= BOARD_MAX_SIZE; i++) {
+			board = new Board(i);
+			char[][] helpstate = makeHelpState(board);
+			helpstate[0][0] = 'X';
+			helpstate[helpstate.length - 1][0] = 'X';
+			helpstate[0][helpstate[0].length - 1] = 'X';
+			helpstate[helpstate.length - 1][helpstate[0].length - 1] = 'X';
+			helpstate[3][8] = 'X';
+			helpstate[8][11] = 'X';
+			board.setBoardstate(helpstate);
+			board.printBoard();
+		}
 	}
 
 	/**
@@ -52,15 +65,18 @@ public class BoardTest {
 	 */
 	@Test
 	public void printBoardTest3() {
-		Board board = new Board(boardWidth);
-		char[][] helpstate = new char[board.getBoardstate().length][board.getBoardstate()[0].length];
-		for (int i = 0; i < helpstate.length; i++) {
-			for (int j = 0; j < helpstate[i].length; j++) {
-				helpstate[i][j] = 'X';
+		Board board;
+		for (int i = BOARD_MIN_SIZE; i <= BOARD_MAX_SIZE; i++) {
+			board = new Board(i);
+			char[][] helpstate = makeHelpState(board);
+			for (int j = 0; j < helpstate.length; j++) {
+				for (int k = 0; k < helpstate[j].length; k++) {
+					helpstate[j][k] = 'X';
+				}
 			}
+			board.setBoardstate(helpstate);
+			board.printBoard();
 		}
-		board.setBoardstate(helpstate);
-		board.printBoard();
 	}
 
 	/**
@@ -68,8 +84,11 @@ public class BoardTest {
 	 */
 	@Test
 	public void isValidMoveTest1() {
-		Board board = new Board(boardWidth);
-		assertTrue(board.isValidMove("a1"));
+		Board board;
+		for (int i = BOARD_MIN_SIZE; i <= BOARD_MAX_SIZE; i++) {
+			board = new Board(i);
+			assertTrue(board.isValidMove("a1"));
+		}
 	}
 
 	/**
@@ -77,16 +96,19 @@ public class BoardTest {
 	 */
 	@Test
 	public void isValidMoveTest2() {
-		Board board = new Board(boardWidth);
-		char[][] helpstate = new char[board.getBoardstate().length][board.getBoardstate()[0].length];
-		helpstate[2][3] = 'B';
-		helpstate[5][2] = 'B';
-		helpstate[7][14] = 'B';
-		helpstate[8][5] = 'B';
-		helpstate[10][10] = 'B';
-		helpstate[13][1] = 'B';
-		board.setBoardstate(helpstate);
-		assertFalse(board.isValidMove("c4"));
+		Board board;
+		for (int i = BOARD_MIN_SIZE; i <= BOARD_MAX_SIZE; i++) {
+			board = new Board(i);
+			char[][] helpstate = makeHelpState(board);
+			helpstate[2][3] = 'B';
+			helpstate[5][2] = 'B';
+			helpstate[7][14] = 'B';
+			helpstate[8][5] = 'B';
+			helpstate[10][10] = 'B';
+			helpstate[13][1] = 'B';
+			board.setBoardstate(helpstate);
+			assertFalse(board.isValidMove("c4"));
+		}
 	}
 
 	/**
@@ -94,101 +116,106 @@ public class BoardTest {
 	 */
 	@Test
 	public void isValidMoveTest3() {
-		Board board = new Board(boardWidth);
+		Board board = new Board(BOARD_MIN_SIZE);
 		assertFalse(board.isValidMove("a35"));
 		assertFalse(board.isValidMove("z2"));
 		assertFalse(board.isValidMove("z35"));
 		assertFalse(board.isValidMove("ab"));
+		assertFalse(board.isValidMove(""));
 		assertFalse(board.isValidMove("b"));
 		assertFalse(board.isValidMove("4"));
 		assertFalse(board.isValidMove("55"));
 	}
 
 	/**
-	 * Test-ID Board15
+	 * Test-ID Board9-1
 	 */
 	@Test
-	public void isRunningTest1() {
-		Board board = new Board(boardWidth);
-		board.isRunning = true;
-		assertTrue(board.isRunning());
+	public void isValidMoveTest4() {
+		Board board = new Board(BOARD_MIN_SIZE);
+		assertFalse(board.isValidMove("a16"));
+		board = new Board(BOARD_MAX_SIZE);
+		assertTrue(board.isValidMove("a16"));
 	}
 
-	/**
-	 * Test-ID Board16
-	 */
-	@Test
-	public void isRunningTest2() {
-		Board board = new Board(boardWidth);
-		board.isRunning = false;
-		assertFalse(board.isRunning());
-	}
+//	/**
+//	 * Test-ID Board15
+//	 */
+//	@Test
+//	public void isRunningTest1() {
+//		Board board = new Board(BOARD_MIN_SIZE);
+//		board.isRunning = true;
+//		assertTrue(board.isRunning());
+//	}
 
-	/**
-	 * Test-ID Board17
-	 */
-	@Test
-	public void whoWonTest1() {
-		Board board = new Board(boardWidth);
-		board.whoWon = true;
-		assertTrue(board.whoWon());
-	}
+//	/**
+//	 * Test-ID Board16
+//	 */
+//	@Test
+//	public void isRunningTest2() {
+//		Board board = new Board(BOARD_MIN_SIZE);
+//		board.isRunning = false;
+//		assertFalse(board.isRunning());
+//	}
 
-	/**
-	 * Test-ID Board18
-	 */
-	@Test
-	public void whoWonTest2() {
-		Board board = new Board(boardWidth);
-		board.whoWon = false;
-		assertFalse(board.whoWon());
-	}
+//	/**
+//	 * Test-ID Board17
+//	 */
+//	@Test
+//	public void whoWonTest1() {
+//		Board board = new Board(BOARD_MIN_SIZE);
+//		board.whoWon = true;
+//		assertTrue(board.whoWon());
+//	}
+
+//	/**
+//	 * Test-ID Board18
+//	 */
+//	@Test
+//	public void whoWonTest2() {
+//		Board board = new Board(BOARD_MIN_SIZE);
+//		board.whoWon = false;
+//		assertFalse(board.whoWon());
+//	}
 
 	/**
 	 * Test-ID Board33
 	 */
 	@Test
 	public void convertCoordinatesTest1() {
-		Board board = new Board(boardWidth);
-		int[] expected = { 0, 0 };
+		Board board = new Board(BOARD_MIN_SIZE);
+		int[] expected = { 1, 1 };
 		assertTrue(Arrays.equals(board.convertCoordinate("a1"), expected));
-		expected[0] = 0;
-		expected[1] = 14;
+		expected[0] = 1;
+		expected[1] = 15;
 		assertTrue(Arrays.equals(board.convertCoordinate("a15"), expected));
-		expected[0] = 14;
-		expected[1] = 0;
+		expected[0] = 15;
+		expected[1] = 1;
 		assertTrue(Arrays.equals(board.convertCoordinate("o1"), expected));
-		expected[0] = 14;
-		expected[1] = 14;
+		expected[0] = 15;
+		expected[1] = 15;
 		assertTrue(Arrays.equals(board.convertCoordinate("o15"), expected));
-		expected[0] = 0;
-		expected[1] = 0;
+		expected[0] = 1;
+		expected[1] = 1;
 		assertTrue(Arrays.equals(board.convertCoordinate("A1"), expected));
-		expected[0] = 0;
-		expected[1] = 14;
+		expected[0] = 1;
+		expected[1] = 15;
 		assertTrue(Arrays.equals(board.convertCoordinate("A15"), expected));
-		expected[0] = 14;
-		expected[1] = 0;
+		expected[0] = 15;
+		expected[1] = 1;
 		assertTrue(Arrays.equals(board.convertCoordinate("O1"), expected));
-		expected[0] = 14;
-		expected[1] = 14;
+		expected[0] = 15;
+		expected[1] = 15;
 		assertTrue(Arrays.equals(board.convertCoordinate("O15"), expected));
 	}
 
 	/**
 	 * Test-ID Board34
 	 */
-	@Test
+	@Test(expected = Exception.class)
 	public void convertCoordinatesTest2() {
-		Board board = new Board(boardWidth);
-		int[] expected = { 15, 33 };
-		assertTrue(Arrays.equals(board.convertCoordinate("p34"), expected));
-		expected[0] = 0;
-		expected[1] = 21;
-		assertTrue(Arrays.equals(board.convertCoordinate("a22"), expected));
-		expected[0] = 18;
-		expected[1] = 1;
-		assertTrue(Arrays.equals(board.convertCoordinate("s2"), expected));
+		Board board = new Board(BOARD_MIN_SIZE);
+		board.convertCoordinate("p34");
 	}
 
 	/**
@@ -196,17 +223,17 @@ public class BoardTest {
 	 */
 	@Test
 	public void convertCoordinatesTest3() {
-		Board board = new Board(boardWidth);
-		int[] expected = { 0, 0 };
+		Board board = new Board(BOARD_MIN_SIZE);
+		int[] expected = { 1, 1 };
 		assertTrue(Arrays.equals(board.convertCoordinate("1a"), expected));
-		expected[0] = 0;
-		expected[1] = 14;
+		expected[0] = 1;
+		expected[1] = 15;
 		assertTrue(Arrays.equals(board.convertCoordinate("15a"), expected));
-		expected[0] = 14;
-		expected[1] = 0;
+		expected[0] = 15;
+		expected[1] = 1;
 		assertTrue(Arrays.equals(board.convertCoordinate("1o"), expected));
-		expected[0] = 14;
-		expected[1] = 14;
+		expected[0] = 15;
+		expected[1] = 15;
 		assertTrue(Arrays.equals(board.convertCoordinate("15o"), expected));
 	}
 
@@ -215,7 +242,7 @@ public class BoardTest {
 	 */
 	@Test(expected = Exception.class)
 	public void convertCoordinatesTest4() {
-		Board board = new Board(boardWidth);
+		Board board = new Board(BOARD_MIN_SIZE);
 		board.convertCoordinate("1234");
 	}
 
@@ -224,7 +251,7 @@ public class BoardTest {
 	 */
 	@Test(expected = Exception.class)
 	public void convertCoordinatesTest5() {
-		Board board = new Board(boardWidth);
+		Board board = new Board(BOARD_MIN_SIZE);
 		board.convertCoordinate("hallo");
 	}
 
@@ -233,7 +260,7 @@ public class BoardTest {
 	 */
 	@Test(expected = Exception.class)
 	public void convertCoordinatesTest6() {
-		Board board = new Board(boardWidth);
+		Board board = new Board(BOARD_MIN_SIZE);
 		board.convertCoordinate("-23a");
 	}
 
@@ -242,7 +269,25 @@ public class BoardTest {
 	 */
 	@Test(expected = Exception.class)
 	public void convertCoordinatesTest7() {
-		Board board = new Board(boardWidth);
+		Board board = new Board(BOARD_MIN_SIZE);
 		board.convertCoordinate("f-3");
+	}
+
+	/**
+	 * Test-ID Board36-5
+	 */
+	@Test(expected = Exception.class)
+	public void convertCoordinatesTest8() {
+		Board board = new Board(BOARD_MIN_SIZE);
+		board.convertCoordinate("1");
+	}
+
+	/**
+	 * Test-ID Board36-6
+	 */
+	@Test(expected = Exception.class)
+	public void convertCoordinatesTest9() {
+		Board board = new Board(BOARD_MIN_SIZE);
+		board.convertCoordinate("");
 	}
 }
